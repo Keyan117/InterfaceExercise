28406e5dcc8384f95bf479ca54c7233f82f7be3b
Keyan117 <kdaneshvari98@gmail.com>
2023-04-12 12:40:39 -0700

    update

---
+++ InterfaceExercise/ICompany.cs
@@ -0,0 +1,9 @@
+using System;
+namespace InterfaceExercise
+{
+    public interface ICompany
+    {
+        public string CompanyName { get; set; }
+        public string Motto { get; set; }
+    }
+}
